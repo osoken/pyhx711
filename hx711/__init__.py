dfa6ad6aa3da9a5653095b7a9be18618861956e5
osoken <osoken.devel@outlook.jp>
2019-12-15 17:43:14 +0900

    v 0.0.1

--- hx711/__init__.py
+++ hx711/__init__.py
@@ -7,6 +7,6 @@ __long_description__ = u'''HX711 Utility
 
 __author__ = u'osoken'
 __email__ = u'osoken.devel@outlook.jp'
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 
 __package_name__ = u'hx711'
